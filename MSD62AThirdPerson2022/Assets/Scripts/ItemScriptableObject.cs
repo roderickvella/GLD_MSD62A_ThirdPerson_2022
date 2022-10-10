47cac466cc9835da886fccdf1891324d3af4770b
roderickvella <55999524+roderickvella@users.noreply.github.com>
2022-10-10 10:47:33 +0200

    Lesson 2

---
+++ MSD62AThirdPerson2022/Assets/Scripts/ItemScriptableObject.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="GameItem",menuName ="Game Items/Create Game item")]
+public class ItemScriptableObject : ScriptableObject
+{
+    public string title;
+    public Sprite icon;
+    public int increaseValue;
+    public Type type; //stores either health or mana (not both at the same time)
+
+
+    public enum Type
+    {
+        Health,
+        Mana
+    }
+}
